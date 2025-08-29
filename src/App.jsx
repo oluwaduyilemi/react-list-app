3414f2ed943c0d95331b686cbcee46e440328c53
oluwaduyilemi <favourbakare24@gmail.com>
2025-08-29 11:28:38 +0100

    Initial commit: React List App with useState

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import UseArray from './Components/UseArray'
+
+function App() {
+
+  return (
+    <>
+        <UseArray/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/ArrayData.jsx
@@ -0,0 +1,7 @@
+// ArrayData.js
+export const ArrayData = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Jane" },
+  { id: 3, name: "Mike" },
+  { id: 4, name: "Mario" }
+]; 
